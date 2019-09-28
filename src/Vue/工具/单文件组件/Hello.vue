e6b4ab35639fa34c1b74475ad4c543fdbaa35300
guoqzuo <guoqzuo@gmail.com>
2019-09-28 23:28:55 +0800

    add Vue 渲染函数_单文件组件部分内容

---
+++ src/Vue/工具/单文件组件/Hello.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>{{ greeting }} World!</p>
+</template>
+
+<script>
+  module.exports = {
+    data: function() {
+      return {
+        greeting: 'Hello'
+      }
+    }
+  }
+</script>
+
+<style>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
