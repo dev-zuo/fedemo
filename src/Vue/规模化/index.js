84763cdc15d6dbd7bd3cc418335c28c430925a92
guoqzuo <guoqzuo@gmail.com>
2019-10-08 21:37:15 +0800

    1. A 新增 混入demo, 规模化-路由相关demo 2. A 新增 .gitignore

---
+++ src/Vue/规模化/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const fs=require("fs");
+const app = express();
+
+app.use('*', function(req, res) {
+  console.log('接收到请求')
+  console.log(req.baseUrl)
+
+  res.sendfile('0_从零开始简单的路由.html')
+})
+
+app.listen(3000, function() {  
+    console.log("server start at 127.0.0.1:3000");
+});
