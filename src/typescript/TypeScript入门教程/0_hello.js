9ffc92ef75c81f85753b91a5c89514794de885dd
guoqzuo <guoqzuo@gmail.com>
2019-12-26 19:47:20 +0800

    typescript 入门demo

---
+++ src/typescript/TypeScript入门教程/0_hello.js
@@ -0,0 +1,5 @@
+function sayHello(name) {
+    return 'Hello, ' + name;
+}
+sayHello('Tom');
+sayHello(123);
---
+++ src/typescript/TypeScript入门教程/0_hello.ts
@@ -0,0 +1,8 @@
+
+function sayHello(name: string) {
+  return 'Hello, ' + name
+}
+
+sayHello('Tom')
+sayHello(123)
+
