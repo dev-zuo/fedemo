f7d3def31673526b5ff1b730d1919ffa54bedf67
guoqzuo <guoqzuo@gmail.com>
2020-03-12 21:56:16 +0800

    update hover改变其他元素样式,vuecli demo测试el-dialog组件使用v-model来封装

---
+++ src/vuecli-demo/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <user-selection v-model="showUserSelection" @confirm="confirm"/>
+    <el-button type="primary" @click="showUserSelection = true">打开弹窗</el-button>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    UserSelection: () => import("../src/components/user-selection/src/main")
+  },
+  data() {
+    return {
+      showUserSelection: false
+    }
+  },
+  methods: {
+    confirm(value) {
+      console.log(value)
+    }
+  }
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/vuecli-demo/src/components/user-selection/src/main.vue
@@ -0,0 +1,35 @@
+
+<template>
+  <div>
+    <el-dialog title="提示" :visible.sync="dialogVisible">
+      内容
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+<script>
+export default {
+  model: {
+    prop: 'show', // 设置对应v-model的属性字段
+    event: 'close' // 如果不指定默认为input，当$emit该事件，可以自动执行 修改父组件v-model参数的值
+  },
+  props: ['show'], // 接收v-model的传值
+  computed: {
+    dialogVisible: {
+      get() {
+        return this.show
+      },
+      set(newVal) {
+        console.log(newVal)
+        this.$emit('close', newVal)
+      }
+    }
+  },
+  data() {
+    return {}
+  }
+}
+</script>
