7a53ffec7f1f729072f5681aee56478c2e573e87
guoqzuo <guoqzuo@gmail.com>
2020-05-11 13:16:27 +0800

    update error

--- src/vuecli-demo/src/views/infiniteScrollTest/subComps.vue
+++ src/vuecli-demo/src/views/infiniteScrollTest/subComps.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- v-infinite-scroll 需要先绑定一些事件，然后监听当前组件mounted，开始绑定事件 -->
-    <!-- 但问题是，如果嵌套组件，v-infinite-scroll 还没初开始绑定逻辑，当前组件就已经mounted完了.... --->
+    <!-- 但问题是，如果嵌套组件，v-infinite-scroll 还没初开始绑定逻辑，当前组件就已经mounted完了.... -->
     <sub-comps-middle>
       <div v-infinite-scroll="loadMore" infinite-scroll-distance="10">
         测试
