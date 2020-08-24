616962c8e590ca793b088955f774b82706a94fa3
guoqzuo <guoqzuo@gmail.com>
2020-08-25 00:54:27 +0800

    update eslint-fix-test demo

---
+++ src/DebugDemo/eslint-fix-test/test.js
@@ -0,0 +1,7 @@
+var a = 1;
+
+function test() {
+  console.log("test");
+}
+
+console.log(a, test());
---
+++ src/DebugDemo/eslint-fix-test/test.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    水电费水电费
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
