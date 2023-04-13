3afd1ea36654427752e6b6ea6885d68c7ce4d84f
zuoxiaobai <guoqzuo@gmail.com>
2023-04-14 05:56:31 +0800

    feat: update sse demo

--- src/DebugDemo/server-send-events/index.js
+++ src/DebugDemo/server-send-events/index.js
@@ -9,6 +9,7 @@ const router = new KoaRouter();
 
 // 响应给前端 ContentType: text/event-stream
 // https://developer.mozilla.org/zh-CN/docs/Web/API/Server-sent_events/Using_server-sent_events#%E4%BB%8E%E6%9C%8D%E5%8A%A1%E5%99%A8%E6%8E%A5%E5%8F%97%E4%BA%8B%E4%BB%B6
+// Koa SSE 实现 https://stackoverflow.com/questions/53295738/koa-sse-connection-reconnecting
 router.get("/open-ai/sendMsg", async (ctx) => {
     ctx.type = "text/event-stream"
     ctx.set({
@@ -18,16 +19,16 @@ router.get("/open-ai/sendMsg", async (ctx) => {
     })
     const steamData = new PassThrough()
     ctx.body = steamData
-    let i = 20;
+    let i = 1;
     let timer = setInterval(() =>{
-        if (i === 0) {
+        if (i === 21) {
             steamData.write(`data: [DONE]\n\n`)
             clearInterval(timer)
         } else {
             steamData.write(`data: ${i}\n\n`)
         }
-        i--;
-    }, 1000)
+        i++;
+    }, 500)
 })
 app.use(new KoaStatic(path.resolve(__dirname, './fe')))
 app.use(router.routes()).use(router.allowedMethods());
